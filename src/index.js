66c1dd9ae42d494063abae62231a8ea91b13c202
gegorov <gegorov@yahoo.com>
2018-10-07 18:06:48 +0300

    feat: add implementation of SmartCalculator class

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue;
   }
 
   add(number) {
-    // your implementation
+    this.value = `${this.value}+${number}`;
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.value = `${this.value}-${number}`;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.value = `${this.value}*${number}`;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.value = `${this.value}/${number}`;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.value = `${this.value}^${number}`;
+    return this;
+  }
+
+  toString() {
+    return eval(this.value); // eslint-disable-line
   }
 }
 
